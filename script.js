9df7e52267a28688e0b49dba2f734238aa5b097c
SoshuKo <chitian.kokkota@gmail.com>
2024-10-07 02:35:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const headConsonants = ["m", "p", "pr", "b", "br", "f", "fr", "v", "n", "t", "tr", "d", "dr", "c", "cl", "s", "sl", "z", "r", "l", "ch", "chr", "sh", "shr", "j", "y", "k", "kr", "g", "gr", "x", "xr", "w", ""];
+const firstSyllableConsonants = ["m", "p", "pr", "sp", "spr", "b", "br", "f", "fr", "v", "n", "t", "tr", "st", "str", "d", "dr", "c", "cl", "sc", "scl", "s", "sl", "z", "r", "l", "ch", "chr", "sch", "schr", "sh", "shr", "j", "ņ", "ç", "çļ", "sç", "sçļ", "ģ", "ģl", "ç", "çļ", "çh", "çhļ", "y", "ļ", "k", "kr", "sk", "skr", "g", "gr", "x", "xr", "w", ""];
+const otherSyllableConsonants = ["m", "p", "pr", "b", "br", "f", "fr", "v", "n", "t", "tr", "d", "dr", "c", "cl", "s", "sl", "z", "r", "l", "ch", "chr", "sh", "shr", "j", "y", "k", "kr", "g", "gr", "x", "xr", "w", ""];
 const specialConsonants = ["pr", "br", "fr", "tr", "dr", "cl", "sl", "chr", "shr", "kr", "gr", "xr"];
 const maleVowels = ["ï", "u", "ë", "o", "a"];
 const femaleVowels = ["i", "ü", "e", "ö", "a"];
@@ -9,8 +10,9 @@ function getRandomElement(arr) {
     return arr[Math.floor(random() * arr.length)];
 }
 
-function getRandomHeadConsonant() {
-    return random() < 1/3 ? getRandomElement(specialConsonants) : getRandomElement(headConsonants);
+function getRandomHeadConsonant(isFirstSyllable) {
+    const consonants = isFirstSyllable ? firstSyllableConsonants : otherSyllableConsonants;
+    return random() < 1/3 ? getRandomElement(specialConsonants) : getRandomElement(consonants);
 }
 
 function isFemaleWord(headConsonant) {
@@ -86,7 +88,7 @@ function convertToFemale(word) {
 
 function generateWord() {
     let word = "";
-    let headConsonant = getRandomElement(headConsonants);
+    let headConsonant = getRandomHeadConsonant(true);
     word += headConsonant;
 
     let isFemale = isFemaleWord(headConsonant);
@@ -104,7 +106,48 @@ function generateWord() {
         word += getRandomElement(endConsonants);
     }
 
-    const syllableCount =
+    const syllableCount = document.getElementById("syllableCount").value;
+    const maxSyllables = syllableCount === "random" ? (random() < 1/3 ? 2 : (random() < 1/5 ? 3 : 1)) : parseInt(syllableCount);
+
+    for (let i = 1; i < maxSyllables; i++) {
+        let nextHeadConsonant = getRandomHeadConsonant(false);
+        if (nextHeadConsonant === "") {
+            nextHeadConsonant = "'";
+        }
+        if (isFemale) {
+            nextHeadConsonant = replaceFemaleConsonant(nextHeadConsonant);
+        }
+        word += nextHeadConsonant;
+
+        let nextVowel = getRandomElement(vowels);
+        if (!hasLongVowel && random() < 1/8) {
+            nextVowel += "gh";
+            hasLongVowel = true;
+        }
+        word += nextVowel;
+
+        if (random() < 0.2) {
+            word += getRandomElement(endConsonants);
+        }
+    }
+
+    if (isFemale) {
+        word = convertToFemale(word);
+    } else {
+        word = convertToMale(word);
+    }
+
+    return word;
+}
+
+function generateWords() {
+    const wordCount = document.getElementById("wordCount").value;
+    const generatedWordsDiv = document.getElementById("generatedWords");
+    generatedWordsDiv.innerHTML = "";
+
+    for (let i = 0; i < wordCount; i++) {
+        let word = generateWord();
+        let wordElement = document.createElement("p");
         wordElement.innerText = word;
         generatedWordsDiv.appendChild(wordElement);
     }
